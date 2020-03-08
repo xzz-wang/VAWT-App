6aa996e04a48fe3ee218e6f4dbfc2ff3966dcf30
Xuezheng Wang <xzzwang@icloud.com>
2020-03-08 12:53:03 -0700

    Successfully parsed the data fetched from the net

--- src/TestPage.js
+++ src/TestPage.js
@@ -27,7 +27,7 @@ export default class TestPage extends Component {
     super(props);
 
     this.state = {
-      input: '',
+      ip: 'http://128.54.53.100',
       name: "N/A",
       status: "N/A",
       temp: "N/A",
@@ -42,17 +42,21 @@ export default class TestPage extends Component {
         <TextInput
           style={{height: 40, width: 300, flex: 1}}
           placeholder="Type here to input IPAddress!"
-          onChangeText={(ip) => this.setState({input: ip})}
-          value={this.state.text}
+          onChangeText={(ip) => this.setState({ip: ip})}
+          value={this.state.ip}
         />
         <TouchableOpacity
           style={{flex: 1}}
           onPress={() => {
-            fetch(this.state.input)
-            .then((response) => {
-              var obj = JSON.parse(response);
+            fetch(this.state.ip)
+            .then((response) => response.json())
+            .then((data) => {
               this.setState({
-                status: obj.status,
+                name: data.name,
+                status: data.status,
+                temp: data.temp,
+                wind: data.wind,
+                battery: data.battery,
               });
             })
             .catch((error) => {
@@ -63,7 +67,7 @@ export default class TestPage extends Component {
           >
           <Text style={{flex: 1, color: 'blue'}}>Fetch Data</Text>
         </TouchableOpacity>
-        <Text style={{flex: 1}}>IP Address: {this.state.input}</Text>
+        <Text style={{flex: 1}}>IP Address: {this.state.ip}</Text>
         <Text style={{flex: 1}}>name: {this.state.name}</Text>
         <Text style={{flex: 1}}>status: {this.state.status}</Text>
         <Text style={{flex: 1}}>temp: {this.state.temp}</Text>
